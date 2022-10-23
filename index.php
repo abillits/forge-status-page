6f3db77a2c78fdc8dbbe598b64f03ddbd45ff6d9
andrew <andrew@idtstudios.com>
2022-10-23 15:11:50 -0500

    ..

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 if (file_exists('.env')) {
   $env_vars = file('.env');
   foreach($env_vars as $env_var) {
+    echo $env_var;
     #echo str_replace('export ','',$env_var) . '\n';
     $env_var_pieces = explode('=', str_replace('export ','',$env_var));
     if (!defined($env_var_pieces[0])) {
@@ -9,5 +10,4 @@ if (file_exists('.env')) {
     }
   }
 }
-echo TEST;
 ?>
